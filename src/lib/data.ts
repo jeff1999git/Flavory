a84fa9c80785236b13889470e69eaa9562da2995
Jeff Joe <jeffdevelop1@gmail.com>
2025-06-27 21:01:12 +0000

    in home page product replace the image link with this: https://res.cloud

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,7 +6,7 @@ export const products: Product[] = [
     name: 'Green chilli powder',
     price: 70,
     description: 'Rich in vitamin C, vitamin A, iron, and antioxidants.',
-    imageUrl: 'https://res.cloudinary.com/doelglurk/image/upload/v1750876861/20250625_2359_Green_Chilli_Pouch_Design_remix_01jym5h04aev0b2vjg7ctdxcrv-removebg-preview_zkuuk0.png',
+    imageUrl: 'https://res.cloudinary.com/doelglurk/image/upload/v1751051734/20250625_2359_Green_Chilli_Pouch_Design_remix_01jym5h04aev0b2vjg7ctdxcrv-removebg-preview_zkuuk0.png',
     dataAiHint: 'chilli powder',
     details: {
       title: 'Green Chilli Powder',
